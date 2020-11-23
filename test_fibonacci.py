8951b51add0c3de4c1279ba18a48c9713055844f
maciejg <einstein1905meister@gmail.com>
2020-11-23 22:01:18 +0100

    Adding tests for fibonacci number from the beginning of the sqauence

--- test_fibonacci.py
+++ test_fibonacci.py
@@ -8,3 +8,15 @@ def test_fib_0_is_0_edge_case():
 def test_fib_1_is_1_edge_case():
     assert fib(1) == 1
 
+
+def test_fib_for_10():
+    assert fib(10) == 55
+
+
+def test_fib_for_12():
+    assert fib(12) == 144
+
+
+def test_fib_for_20():
+    assert fib(20) == 6765
+
