3b163f10310b8201a36b2ab1f742cab939857af6
maciejg <einstein1905meister@gmail.com>
2020-11-23 22:14:53 +0100

    Refactor tests using parametrize

--- test_fibonacci.py
+++ test_fibonacci.py
@@ -1,22 +1,16 @@
+import pytest
 from fibonacci.fibonacci import fib
 
 
-def test_fib_0_is_0_edge_case():
-    assert fib(0) == 0
-
-
-def test_fib_1_is_1_edge_case():
-    assert fib(1) == 1
-
-
-def test_fib_for_10():
-    assert fib(10) == 55
-
-
-def test_fib_for_12():
-    assert fib(12) == 144
-
-
-def test_fib_for_20():
-    assert fib(20) == 6765
-
+@pytest.mark.parametrize(
+    'fib_index, result',
+    [
+        (0, 0),
+        (1, 1),
+        (10, 55),
+        (12, 144),
+        (20, 6765)
+    ]
+)
+def test_fib_0_is_0_edge_case(fib_index, result):
+    assert fib(fib_index) == result
